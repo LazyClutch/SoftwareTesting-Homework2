904bfec8beaab0d7bf07954bc8bc47b8fbc6ddc0
Lazy <lr_5146@163.com>
2015-04-26 15:55:18 +0800

    Initial Commit

---
+++ SoftwareTesting-Homework2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoftwareTesting-Homework2
+//
+//  Created by Lazy on 15/4/26.
+//  Copyright (c) 2015年 Lazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoftwareTesting-Homework2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoftwareTesting-Homework2
+//
+//  Created by Lazy on 15/4/26.
+//  Copyright (c) 2015年 Lazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
